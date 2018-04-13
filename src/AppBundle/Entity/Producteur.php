5216dbe603be9d826799cfb2800638a53d094427
DavcoDev <david.cossedu@gmail.com>
2018-04-13 12:05:36 +0200

    First commit

---
+++ src/AppBundle/Entity/Producteur.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Producteur
+ *
+ * @ORM\Table(name="producteur")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProducteurRepository")
+ */
+class Producteur
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nomProducteur", type="string", length=255)
+     */
+    private $nomProducteur;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nomFerme", type="string", length=255)
+     */
+    private $nomFerme;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string", length=255)
+     */
+    private $ville;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nomProducteur
+     *
+     * @param string $nomProducteur
+     *
+     * @return Producteur
+     */
+    public function setNomProducteur($nomProducteur)
+    {
+        $this->nomProducteur = $nomProducteur;
+
+        return $this;
+    }
+
+    /**
+     * Get nomProducteur
+     *
+     * @return string
+     */
+    public function getNomProducteur()
+    {
+        return $this->nomProducteur;
+    }
+
+    /**
+     * Set nomFerme
+     *
+     * @param string $nomFerme
+     *
+     * @return Producteur
+     */
+    public function setNomFerme($nomFerme)
+    {
+        $this->nomFerme = $nomFerme;
+
+        return $this;
+    }
+
+    /**
+     * Get nomFerme
+     *
+     * @return string
+     */
+    public function getNomFerme()
+    {
+        return $this->nomFerme;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     *
+     * @return Producteur
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    /**
+     * Get ville
+     *
+     * @return string
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+}
+
---
+++ src/AppBundle/Entity/Produit.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Produit
+ *
+ * @ORM\Table(name="produit")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nomProduit", type="string", length=255)
+     */
+    private $nomProduit;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantite", type="integer")
+     */
+    private $quantite;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nomProduit
+     *
+     * @param string $nomProduit
+     *
+     * @return Produit
+     */
+    public function setNomProduit($nomProduit)
+    {
+        $this->nomProduit = $nomProduit;
+
+        return $this;
+    }
+
+    /**
+     * Get nomProduit
+     *
+     * @return string
+     */
+    public function getNomProduit()
+    {
+        return $this->nomProduit;
+    }
+
+    /**
+     * Set quantite
+     *
+     * @param integer $quantite
+     *
+     * @return Produit
+     */
+    public function setQuantite($quantite)
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    /**
+     * Get quantite
+     *
+     * @return int
+     */
+    public function getQuantite()
+    {
+        return $this->quantite;
+    }
+}
+
